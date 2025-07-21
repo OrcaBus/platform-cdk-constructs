363446c9e387651f911eba51a5416c2e53cb5a8e
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-07-21 13:12:23 +1000

    Added update multiqc job status function to fastq multiqc status tools

--- packages/lambda/layers/orcabus_api_tools/src/orcabus_api_tools/fastq/multiqc_helpers.py
+++ packages/lambda/layers/orcabus_api_tools/src/orcabus_api_tools/fastq/multiqc_helpers.py
@@ -3,8 +3,11 @@
 """
 Run MultiQC on a list of fastq ids
 """
-from typing import List
 
+# Standard imports
+from typing import List, Optional
+
+# Custom imports
 from orcabus_api_tools.fastq import fastq_patch_request
 from orcabus_api_tools.fastq.globals import MULTIQC_ENDPOINT
 
@@ -18,4 +21,23 @@ def run_multiqc(fastq_id_list: List[str]) -> str:
     return fastq_patch_request(
         f"{MULTIQC_ENDPOINT}",
         json_data=fastq_id_list
+    )
+
+
+def update_multiqc_job_status(
+        job_id: str,
+        status: str,
+        steps_execution_arn: Optional[str] = None,
+        output_uri: Optional[str] = None
+):
+    return fastq_patch_request(
+        f"{MULTIQC_ENDPOINT}/{job_id}",
+        json_data=dict(filter(
+            lambda kv_iter_: kv_iter_[1] is not None,
+            {
+                "status": status,
+                "stepsExecutionArn": steps_execution_arn,
+                "multiqcOutputHtmlUri": output_uri
+            }.items())
+        )
     )
