2cead7eb1848ba13a4fc097f3f9f5174f88c38e7
Alexis Lucattini <alexis.lucattini@umccr.org>
2025-09-09 08:12:25 +1000

    Bugfix cache configuration files (#146)

--- packages/lambda/layers/icav2_tools/src/icav2_tools/__init__.py
+++ packages/lambda/layers/icav2_tools/src/icav2_tools/__init__.py
@@ -15,6 +15,14 @@ from .aws_helpers import (
 )
 from .globals import ICAV2_BASE_URL
 
+# We also want to set up our environment variables as global variables, so that future calls
+# On the same lambda do not need to re-initialize these variables
+# While we use the lambda params-and-secrets secret extension, this does not work
+# When we need to list params in a path
+STORAGE_CONFIGURATION_FILE = None
+PROJECT_TO_STORAGE_CONFIGURATION_MAPPING_FILE = None
+STORAGE_CREDENTIAL_FILE = None
+
 
 def create_storage_configuration_file() -> Path:
     storage_configuration_file = Path(NamedTemporaryFile(
@@ -29,6 +37,13 @@ def create_storage_configuration_file() -> Path:
             file_h
         )
     return storage_configuration_file
+
+
+def get_storage_configuration_file() -> Path:
+    global STORAGE_CONFIGURATION_FILE
+    if STORAGE_CONFIGURATION_FILE is None:
+        STORAGE_CONFIGURATION_FILE = create_storage_configuration_file()
+    return STORAGE_CONFIGURATION_FILE
 
 
 def create_project_to_storage_configuration_mapping_file() -> Path:
@@ -46,6 +61,13 @@ def create_project_to_storage_configuration_mapping_file() -> Path:
     return project_to_storage_configuration_file
 
 
+def get_project_to_storage_configuration_mapping_file() -> Path:
+    global PROJECT_TO_STORAGE_CONFIGURATION_MAPPING_FILE
+    if PROJECT_TO_STORAGE_CONFIGURATION_MAPPING_FILE is None:
+        PROJECT_TO_STORAGE_CONFIGURATION_MAPPING_FILE = create_project_to_storage_configuration_mapping_file()
+    return PROJECT_TO_STORAGE_CONFIGURATION_MAPPING_FILE
+
+
 def create_storage_credential_file() -> Path:
     storage_credential_file = Path(NamedTemporaryFile(
         delete=False,
@@ -61,6 +83,13 @@ def create_storage_credential_file() -> Path:
     return storage_credential_file
 
 
+def get_storage_credential_file() -> Path:
+    global STORAGE_CREDENTIAL_FILE
+    if STORAGE_CREDENTIAL_FILE is None:
+        STORAGE_CREDENTIAL_FILE = create_storage_credential_file()
+    return STORAGE_CREDENTIAL_FILE
+
+
 def set_icav2_env_vars():
     """
     Set the environment variables for the ICAV2 API
@@ -71,9 +100,9 @@ def set_icav2_env_vars():
     environ["ICAV2_ACCESS_TOKEN"] = get_icav2_access_token()
 
     # Configuration files needed to map storage configurations
-    environ["ICAV2_STORAGE_CONFIGURATION_LIST_FILE"] = str(create_storage_configuration_file())
-    environ["ICAV2_PROJECT_TO_STORAGE_CONFIGURATION_MAPPING_LIST_FILE"] = str(create_project_to_storage_configuration_mapping_file())
-    environ["ICAV2_STORAGE_CREDENTIAL_LIST_FILE"] = str(create_storage_credential_file())
+    environ["ICAV2_STORAGE_CONFIGURATION_LIST_FILE"] = str(get_storage_configuration_file())
+    environ["ICAV2_PROJECT_TO_STORAGE_CONFIGURATION_MAPPING_LIST_FILE"] = str(get_project_to_storage_configuration_mapping_file())
+    environ["ICAV2_STORAGE_CREDENTIAL_LIST_FILE"] = str(get_storage_credential_file())
 
 
 __all__ = [
