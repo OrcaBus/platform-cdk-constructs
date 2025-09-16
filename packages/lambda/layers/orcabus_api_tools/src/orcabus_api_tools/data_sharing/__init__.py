085f4077913dad5d28d8cd322c0313e156b563bc
Felipe Jiménez-Ibarra <62123927+felipeji@users.noreply.github.com>
2025-09-16 15:13:09 +1000

    Feat: add create_package and push_package helpers (#151)
    * Add create_package and push_package helpers to data_sharing.
    
    * Use post_request from utils.requests_helpers.
    
    * Address first round of PR's feedback on data-sharing helpers.
    
    ---------
    
    Co-authored-by: Alexis Lucattini <alexis.lucattini@umccr.org>

--- packages/lambda/layers/orcabus_api_tools/src/orcabus_api_tools/data_sharing/__init__.py
+++ packages/lambda/layers/orcabus_api_tools/src/orcabus_api_tools/data_sharing/__init__.py
@@ -1,8 +1,14 @@
 # FIMXE Implement the following imports
-from typing import Optional, Dict
+from typing import Optional, List, Dict, TypedDict, NotRequired
 
 from .globals import DATA_SHARING_SUBDOMAIN_NAME
-from ..utils.requests_helpers import get_url, get_request, get_request_response_results, patch_request
+from ..utils.requests_helpers import (
+    get_url,
+    get_request,
+    get_request_response_results,
+    patch_request,
+    post_request,
+)
 
 
 # Get url for the subdomain
@@ -20,22 +26,18 @@ def get_data_sharing_url(endpoint: str) -> str:
 
 # Wrappers
 def get_data_sharing_request(
-        endpoint: str,
-        params: Optional[Dict] = None,
+    endpoint: str,
+    params: Optional[Dict] = None,
 ):
-    return get_request(
-        url=get_data_sharing_url(endpoint),
-        params=params
-    )
+    return get_request(url=get_data_sharing_url(endpoint), params=params)
 
 
 def get_data_sharing_request_response_results(
-        endpoint: str,
-        params: Optional[Dict] = None,
+    endpoint: str,
+    params: Optional[Dict] = None,
 ):
     return get_request_response_results(
-        url=get_data_sharing_url(endpoint),
-        params=params
+        url=get_data_sharing_url(endpoint), params=params
     )
 
 
@@ -43,26 +45,57 @@ def data_sharing_patch_request(
     endpoint: str,
     json_data: Optional[Dict] = None,
 ):
-    return patch_request(
-        url=get_data_sharing_url(endpoint),
-        json_data=json_data
-    )
+    return patch_request(url=get_data_sharing_url(endpoint), json_data=json_data)
 
 
 def data_sharing_post_request(
     endpoint: str,
     json_data: Optional[Dict] = None,
 ):
-    return patch_request(
-        url=get_data_sharing_url(endpoint),
-        json_data=json_data
+    return patch_request(url=get_data_sharing_url(endpoint), json_data=json_data)
+
+
+# Models
+class PackageRequestDict(TypedDict):
+    libraryIdList: List[str]
+    dataTypeList: List[str]
+    portalRunIdList: NotRequired[List[str]]
+    defrostArchivedFastqs: NotRequired[bool]
+    useWorkflowFilters: NotRequired[bool]
+    instrumentRunIdList: NotRequired[List[str]]
+
+
+def create_package(
+    package_name: str,
+    package_request: PackageRequestDict,
+) -> Dict:
+    response = post_request(
+        json_data={
+            "packageName": package_name,
+            "packageRequest": package_request,
+        },
+        url=get_data_sharing_url("/api/v1/package"),
     )
 
+    return response
 
-from .query_helpers import (
-    get_package,
-    get_push_job
-)
+
+def push_package(
+    package_id: str,
+    location_uri: str,
+) -> Dict:
+
+    response = post_request(
+        json_data={
+            "shareDestination": location_uri,
+        },
+        url=get_data_sharing_url(f"/api/v1/package/{package_id}:push"),
+    )
+
+    return response
+
+
+from .query_helpers import get_package, get_push_job
 
 from .update_helpers import (
     update_package_status,
@@ -74,6 +107,9 @@ __all__ = [
     "get_package",
     "get_push_job",
     # Push job helpers
+    "push_package",
     "update_package_status",
     "update_push_job_status",
+    # Package helpers
+    "create_package",
 ]
