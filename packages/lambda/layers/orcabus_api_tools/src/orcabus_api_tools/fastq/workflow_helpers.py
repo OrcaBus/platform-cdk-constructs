72ef73e9f3a4681dbb1bd23e7481a61079e0f690
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-12-11 12:56:19 +1100

    Add file + fastq query parameters
    Allows collection of file from ingest id plus more
    Update fastq list row helpers to support bucket and key prefix inputs

--- packages/lambda/layers/orcabus_api_tools/src/orcabus_api_tools/fastq/workflow_helpers.py
+++ packages/lambda/layers/orcabus_api_tools/src/orcabus_api_tools/fastq/workflow_helpers.py
@@ -7,7 +7,7 @@ Workflow helpers - a collection of helper functions for the workflow
 """
 
 # Standard library imports
-from typing import List
+from typing import List, Optional
 
 # Local imports
 from . import get_fastq_request
@@ -15,12 +15,35 @@ from .globals import FASTQ_ENDPOINT, FASTQ_SET_ENDPOINT
 from .models import FastqListRowDict
 
 
-def to_fastq_list_row(fastq_id) -> FastqListRowDict:
+def to_fastq_list_row(
+        fastq_id: str,
+        bucket: Optional[str] = None,
+        key_prefix: Optional[str] = None,
+) -> FastqListRowDict:
     return get_fastq_request(
-        f"{FASTQ_ENDPOINT}/{fastq_id}/toFastqListRow"
+        f"{FASTQ_ENDPOINT}/{fastq_id}/toFastqListRow",
+        params=dict(filter(
+            lambda param_iter_: param_iter_[1] is not None,
+            {
+                "bucket": bucket,
+                "keyPrefix": key_prefix,
+            }.items()
+        ))
     )
 
-def to_fastq_list_rows(fastq_set_id: str) -> List[FastqListRowDict]:
+
+def to_fastq_list_rows(
+        fastq_set_id: str,
+        bucket: Optional[str] = None,
+        key_prefix: Optional[str] = None,
+) -> List[FastqListRowDict]:
     return get_fastq_request(
-        f"{FASTQ_SET_ENDPOINT}/{fastq_set_id}/toFastqListRows"
+        f"{FASTQ_SET_ENDPOINT}/{fastq_set_id}/toFastqListRows",
+        params=dict(filter(
+            lambda param_iter_: param_iter_[1] is not None,
+            {
+                "bucket": bucket,
+                "keyPrefix": key_prefix,
+            }.items()
+        ))
     )
