178b45c9fea35947b87432b67d5cef3460e62913
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-08-06 20:05:51 +1000

    docs(orcabus_api_tools/deploy_status): fix incorrect docstrings
    Fix documentation in the deploy_status module:
    - Fix get_deploy_status_endpoint docstring (was 'Metadata endpoint')
    - Fix query_helpers module docstring (was referencing wrong functions)
    - Add function docstring for get_all_stacks_summary

--- packages/lambda/layers/orcabus_api_tools/src/orcabus_api_tools/deploy_status/query_helpers.py
+++ packages/lambda/layers/orcabus_api_tools/src/orcabus_api_tools/deploy_status/query_helpers.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-"""
-Query helpers -
-
-get_job_from_job_id
-
-get_unarchiving_job_list
-
-"""
+"""Query helpers for the deploy status service."""
 
 # Type hints
 from typing import List, cast
@@ -22,4 +15,9 @@ from .models import StackEventResponseDict
 
 
 def get_all_stacks_summary() -> List[StackEventResponseDict]:
+    """Retrieve a summary of all CloudFormation stacks and their latest events.
+
+    Returns:
+        A list of StackEventResponseDict objects containing stack status information.
+    """
     return cast(List[StackEventResponseDict], get_request(get_deploy_status_endpoint(GET_ALL_STACKS_ENDPOINT)))
