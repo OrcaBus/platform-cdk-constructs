b401e6e11c280f6b83eaae394f85645b81816be5
Alexis Lucattini <alexis.lucattini@umccr.org>
2026-09-04 06:06:25 +0000

    enhancement: improve workflow metadata helper APIs (#264)
    * feat: Add helpers to get libraries/fastqs on workflow run
    
    * refactor(workflow): Improve workflow intersection by library ID
    
    * refactor(workflow): Improve workflow intersection by Fastq ID (RGID)
    
    * refactor(metadata): Update Library type hint from LibraryBase
    
    * feat(workflow): Refactor get_libraries_from_workflow_run for direct Library retrieval
    
    * docs(workflow): Add docstrings to workflow run data accessors
    
    * perf(workflow): Optimize workflow filtering for rgid using direct readset IDs
    
    * style(workflow): Apply consistent indentation in workflow run filters

--- packages/lambda/layers/orcabus_api_tools/src/orcabus_api_tools/workflow/metadata_helpers.py
+++ packages/lambda/layers/orcabus_api_tools/src/orcabus_api_tools/workflow/metadata_helpers.py
@@ -5,12 +5,20 @@ Get workflows from library id
 """
 
 # Standard imports
+import typing
+from copy import copy
 from typing import List, Optional
 
 # Local imports
 from . import get_workflow_request_response_results
 from .globals import WORKFLOW_RUN_ENDPOINT
 from .models import WorkflowRunDetail
+from .workflow_run_helpers import get_workflow_run
+
+# Type hints
+if typing.TYPE_CHECKING:
+    from ..metadata.models import Library
+    from ..fastq.models import Fastq
 
 
 def get_workflows_from_library_id(library_id: str) -> List[WorkflowRunDetail]:
@@ -27,7 +35,9 @@ def get_workflows_from_library_id(library_id: str) -> List[WorkflowRunDetail]:
     )
 
 
-def get_workflows_from_library_id_list(library_id_list: List[str]) -> List[WorkflowRunDetail]:
+def get_workflows_from_library_id_list(
+        library_id_list: List[str],
+) -> List[WorkflowRunDetail]:
     """
     Use the query libraries__libraryId to get workflows from a list of library ids
     However, we only collect the workflows that are associated will all libraries in the list,
@@ -37,9 +47,11 @@ def get_workflows_from_library_id_list(library_id_list: List[str]) -> List[Workf
     :return:
     """
 
+    # If no libraries, then no workflows
     if len(library_id_list) < 1:
         return []
 
+    # Get all workflows from this library id
     all_workflows_intersected = get_workflows_from_library_id(
         library_id_list[0]
     )
@@ -47,15 +59,16 @@ def get_workflows_from_library_id_list(library_id_list: List[str]) -> List[Workf
     if len(library_id_list) == 1:
         return all_workflows_intersected
 
-    for library_id in library_id_list[1:]:
-        workflows_for_library = get_workflows_from_library_id(library_id)
-        all_workflows_intersected = list(filter(
-            lambda workflow_iter_: workflow_iter_['orcabusId'] in list(map(
-                lambda all_workflows_iter_: all_workflows_iter_['orcabusId'],
-                all_workflows_intersected
-            )),
-            workflows_for_library
+    for workflow_iter in all_workflows_intersected:
+        # Get all libraries on run
+        library_ids_on_run = list(map(
+            lambda library_iter_: library_iter_['libraryId'],
+            get_libraries_from_workflow_run(workflow_iter['orcabusId'])
         ))
+
+        # If any library is not on the run, then we remove the run.
+        if not all(library_id in library_ids_on_run for library_id in library_id_list):
+            all_workflows_intersected.remove(workflow_iter)
 
     return all_workflows_intersected
 
@@ -91,20 +104,13 @@ def get_workflows_from_rgid_list(
     if len(fastq_id_list) == 1:
         return all_workflows_intersected
 
-    for fastq_id in fastq_id_list[1:]:
-        workflows_for_fastq_id = get_workflow_request_response_results(
-            WORKFLOW_RUN_ENDPOINT,
-            params={
-                "readsets": fastq_id
-            }
-        )
-        all_workflows_intersected = list(filter(
-            lambda workflow_iter_: workflow_iter_['orcabusId'] in list(map(
-                lambda all_workflows_iter_: all_workflows_iter_['orcabusId'],
-                all_workflows_intersected
-            )),
-            workflows_for_fastq_id
-        ))
+    for workflow_iter in copy(all_workflows_intersected):
+        # Get all libraries on run
+        fastq_id_list_on_run = get_readsets_on_workflow_run(workflow_run_id=workflow_iter['orcabusId'])
+
+        # If any fastq on our list is not on the run, then we remove it
+        if not all(fastq_id in fastq_id_list_on_run for fastq_id in fastq_id_list):
+            all_workflows_intersected.remove(workflow_iter)
 
     return all_workflows_intersected
 
@@ -152,10 +158,10 @@ def get_workflow_runs_from_metadata(
         )
         workflow_run_list = list(filter(
             lambda workflow_run_iter_: (
-                workflow_run_iter_['orcabusId'] in list(map(
-                    lambda workflow_iter_: workflow_iter_['orcabusId'],
-                    workflow_library_id_list
-                ))
+                    workflow_run_iter_['orcabusId'] in list(map(
+                lambda workflow_iter_: workflow_iter_['orcabusId'],
+                workflow_library_id_list
+            ))
             ),
             workflow_run_list
         ))
@@ -167,13 +173,62 @@ def get_workflow_runs_from_metadata(
         )
         workflow_run_list = list(filter(
             lambda workflow_run_iter_: (
-                workflow_run_iter_['orcabusId'] in list(map(
-                    lambda workflow_iter_: workflow_iter_['orcabusId'],
-                    workflow_rgid_list
-                ))
+                    workflow_run_iter_['orcabusId'] in list(map(
+                lambda workflow_iter_: workflow_iter_['orcabusId'],
+                workflow_rgid_list
+            ))
             ),
             workflow_run_list
         ))
 
     # Return the filtered list of workflow runs
     return workflow_run_list
+
+
+def get_readsets_on_workflow_run(
+        workflow_run_id: str
+) -> List[str]:
+    """
+    Returns a list of fastq ids on a run
+    :param workflow_run_id:
+    :return:
+    """
+    return get_workflow_run(workflow_run_orcabus_id=workflow_run_id).get('readsets', [])
+
+
+def get_fastqs_on_workflow_run(
+        workflow_run_id: str
+) -> List['Fastq']:
+    """
+    Get the full fastq objects for each readset (fastq id) on a workflow run
+    :param workflow_run_id:
+    :return:
+    """
+    # Import locally to prevent circular imports
+    from ..fastq import get_fastq
+
+    return list(map(
+        lambda fastq_id_iter_: get_fastq(fastq_id_iter_),
+        get_readsets_on_workflow_run(workflow_run_id=workflow_run_id)
+    ))
+
+
+def get_libraries_from_workflow_run(
+        workflow_run_id: str
+) -> List['Library']:
+    """
+    Get libraries on a workflow run
+    :param workflow_run_id:
+    :return:
+    """
+    from ..metadata import get_libraries_from_library_orcabus_id_list
+
+    # Get the library orcabusIds
+    library_orcabus_id_list = list(set(
+        list(map(
+            lambda fastq_obj_iter_: fastq_obj_iter_['library']['orcabusId'],
+            get_fastqs_on_workflow_run(workflow_run_id)
+        ))
+    ))
+
+    return get_libraries_from_library_orcabus_id_list(library_orcabus_id_list=library_orcabus_id_list)
